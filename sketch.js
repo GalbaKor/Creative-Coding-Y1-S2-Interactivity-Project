8c361a4d221eae31f1609c26bda6dd3caf64f97a
GalbaKor <52763544+GalbaKor@users.noreply.github.com>
2020-02-19 13:20:29 +0000

    moving circles complete

--- sketch.js
+++ sketch.js
@@ -1,155 +1,65 @@
-let Pipes = [];
-
-
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	noFill();
-	/*for(let i = 0; i < Pipenum; i++){
-		Pipes[i] = new Pipe;
-		Pipes[i].move();
-		Pipes[i].display();
-  }*/
-  
-  for( let i = 0; i < Pipes; i++) {
-    Pipes.push(new Pipe());
-  }
-  
-}
-
-function draw() {
-  for( let i = 0; i < Pipes; i++) {
-    Pipes.push(new Pipe());
-    /*Pipes[i].move();*/
-    Pipes[i].display();
-  }
-}
-
-function mousePressed() {
-  Pipes.push();
-}
-
-class Pipe {
-  constructor(){
-    this.x = random(width);
-    this.y = random(height);
-    this.diam = 200;
-    this.red = random(255);
-    this.green = random(255);
-    this.blue = random(255);
-    this.stroke = stroke(this.red,this.green,this.blue);
-    this.speed = random(1);
-    this.xDir = random(-2,2);
-    this.yDir = random(-2,2);
-	let timer = 2;
-  }
-
-
-
-  /*move(){
-    this.timer = round(millis());
-    
-    if (this.timer%88 == 0){
-      this.xDir = random(-2, 2);
-      this.yDir = random(-2, 2);
-    }     
-    this.x += (this.speed * this.xDir);
-    this.y += (this.speed * this.yDir); 
-    
-    if(this.x <= -this.diam || this.x >= width+this.diam || this.y <= -this.diam || this.y >= height+this.diam){
-      this.x = random(width);
-      this.y = random(height);
-      this.red = random(255);
-      this.green = random(255);
-      this.blue = random(255);
-    }    
-  }*/
-  display(){
-    this.stroke = stroke(this.red,this.green,this.blue);
-    ellipse(this.x, this.y, this.diam, this.diam);
-   }
-}
-
-/*
-let Pipe = []; 
-let nbrPipe = 1;
-let pressed = false;
+var pipes = []; // array of Jitter objects
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220);
   noFill();
-  strokeWeight(0.5);
 
-  myButton = createButton('Keep pressing me')
-  .mousePressed(()  => pressed = true)
-  .mouseReleased(() => pressed = false);
-
-  for(let i = 0; i<nbrPipe; i++){
-  Pipe[i] = new Pipeline();
-  
-	if (pressed) i++;
-   }
-
-  
 }
 
-function draw() { 
-  for(let i = 0; i<nbrPipe; i++){
-    Pipe[i].move();    
-    Pipe[i].display();
+// create new object when mouse is pressed
+function mousePressed() {
+
+  var b = new Pipe();
+  pipes.push(b);
+}
+
+function draw() {
+  background(0);
+  // move and display all the objects
+  for (var i = 0; i < pipes.length; i++) {
+    pipes[i].move();
+    pipes[i].display();
   }
-  
 }
 
+// Pipe class
+class Pipe {
 
-class Pipeline{
-  constructor(){
+  constructor() {
     this.x = random(width);
     this.y = random(height);
-    this.diam = 5;
+    this.diameter = 20;
     this.red = random(255);
     this.green = random(255);
     this.blue = random(255);
     this.stroke = stroke(this.red,this.green,this.blue);
-    this.speed = random(1);
+    this.speed = random(0,2);
     this.xDir = random(-2,2);
     this.yDir = random(-2,2);
-	let timer = 2;
-	
-  } 
-   
-  move(){
+  	let timer = 2;
+  }
+
+  move() {    
     this.timer = round(millis());
     
-    //on utilise le modulo, c'est à dire que si le timer est un multiple de 88, alors on entre dans la condition
     if (this.timer%88 == 0){
       this.xDir = random(-2, 2);
       this.yDir = random(-2, 2);
     }     
     this.x += (this.speed * this.xDir);
     this.y += (this.speed * this.yDir); 
-    //print(this.x);
     
-    if(this.x <= -this.diam || this.x >= width+this.diam || this.y <= -this.diam || this.y >= height+this.diam){
+    if(this.x <= -this.diam || this.x >= width+this.diam || this.y <= -this.diam || this.y >= height+this.diam){   // if it goes off the screen, it reappears at a new position.
       this.x = random(width);
       this.y = random(height);
       this.red = random(255);
       this.green = random(255);
-      this.blue = random(255);
-    }    
+      this.blue = random(255); 
+     }
   }
-  
-  display(){
-   this.stroke = stroke(this.red,this.green,this.blue);
-   ellipse(this.x, this.y, this.diam, this.diam);
+
+  display() {
+    ellipse(this.x, this.y, this.diameter);
+    ellipse(this.x, this.y, this.diameter/2);
   }
 }
-
-
-
-
-
-  function windowResized() {
-  	resizeCanvas(windowWidth, windowHeight);
-  }
-*/
